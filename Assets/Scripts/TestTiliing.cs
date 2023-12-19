42a1b62fce1a37805c6cb68d1652bda4602aaf3a
mumu_dev <67819713+MuveloperDev@users.noreply.github.com>
2023-12-19 17:53:02 +0900

    ssss

--- Assets/Scripts/TestTiliing.cs
+++ Assets/Scripts/TestTiliing.cs
@@ -5,21 +5,51 @@ using UnityEngine.UI;
 
 public class TestTiliing : MonoBehaviour
 {
-    public Material material;
+    [SerializeField] private Material _originalMaterial;
+    [SerializeField] private Material _material;
+    [SerializeField] private Image _image;
+    //[SerializeField] private RawImage _image;
+
+    [SerializeField] private const float _defalutValue = 1000;
+    [SerializeField] private float originalAspectRatio;
+    [SerializeField] private float _hp;
     public float tileCount = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        material = GetComponent<Image>().material;
+        _material = new Material(_originalMaterial);
+        _image = GetComponent<Image>();
+        _image.material = _material;
+        _hp = 1500;
+        //_material.mainTexture = _image.mate.texture;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            material.mainTextureScale = new Vector2(tileCount, 1);
-            tileCount += 0.1f;
-        }
+        SetHp(_hp);
+    }
+
+    private void SetHp(float argHp)
+    {
+        // 추후 여기서 Sheild Value 등등 로직 계산.
+        var tileCnt = CalculateTileCnt(argHp);
+
+        //spriteRenderer.material.mainTextureScale = new Vector2(tileCnt, 1);
+        //_material.mainTextureOffset = new Vector2(tileCnt, 1f);
+
+
+        // mainTextureScale의 x 값을 tileCnt로, y 값을 1로 설정
+        _material.mainTextureScale = new Vector2(tileCnt, 1f);
+
+        //_material.SetFloat("_TileX", tileCnt);
+    }
+
+    private float CalculateTileCnt(float argHp)
+    {
+        float value = argHp / _defalutValue;
+        tileCount = value;
+        return value;
     }
 }
